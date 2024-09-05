90d83b4a739079148ea80e231f2d3d56ea87f511
408-10\student <fca2306100009@edu.fca.ac.jp>
2024-09-05 19:43:35 +0900

    タップ方向にいがぐりを投げる

--- Assets/Script/IgaguriGenetator.cs
+++ Assets/Script/IgaguriGenetator.cs
@@ -11,7 +11,9 @@ public class IgaguriGenetator : MonoBehaviour
         if(Input.GetMouseButtonDown(0))
         {
             GameObject igaguri = Instantiate(igaguriPrefab);
-            igaguri.GetComponent<IgaguriController>().Shoot(new Vector3(0, 200, 2000));
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            igaguri.GetComponent<IgaguriController>().Shoot(worldDir.normalized * 2000);
         }
     }
 }
